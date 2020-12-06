b9025b4e7466dab4a84bef6db5ba49f45abc8e16
ruskab <aq0866sw1343de>
2020-12-06 18:36:53 +0100

    #211 assert iterations unit test

--- rup-projects-core/src/test/java/com/core/rupprojectscore/service/ProjectServiceTest.java
+++ rup-projects-core/src/test/java/com/core/rupprojectscore/service/ProjectServiceTest.java
@@ -9,34 +9,44 @@ import org.junit.jupiter.api.Test;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ProjectServiceTest {
 
-    private ProjectService sut;
+    private ProjectService projectService;
     private MemberRepository repository;
     private ProjectDto project;
     private ProjectDto projectDto;
 
     @BeforeEach
     public void before() {
-        sut = new ProjectServiceImpl(new Mapper());
+        projectService = new ProjectServiceImpl(new Mapper());
     }
 
     @Test
     void planProjectTest_having_defaultNumberOfIterations() {
         project = createProjectDto(LocalDate.of(1, 1, 1), LocalDate.of(1, 6, 30), cost(100000L));
-        projectDto = sut.planProject(project);
+        projectDto = projectService.planProject(project);
         assertProject(PhaseType.values(), iterations(10));
         assertPhases(
-                Arrays.asList(1, LocalDate.of(1, 1, 1), LocalDate.of(1, 1, 18)),
-                Arrays.asList(3, LocalDate.of(1, 1, 1), LocalDate.of(1, 1, 18)),
-                Arrays.asList(1, LocalDate.of(1, 1, 1), LocalDate.of(1, 1, 18)),
-                Arrays.asList(1, LocalDate.of(1, 1, 1), LocalDate.of(1, 1, 18))
+                List.of(1, LocalDate.of(1, 1, 1), LocalDate.of(1, 1, 18)),
+                List.of(3, LocalDate.of(1, 1, 19), LocalDate.of(1, 3, 13)),
+                List.of(5, LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(1, LocalDate.of(1, 6, 12), LocalDate.of(1, 6, 29))
+        );
+        assertIterations(
+                List.of(LocalDate.of(1, 1, 1), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 1, 19), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 1, 19), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 1, 19), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11)),
+                List.of(LocalDate.of(1, 3, 14), LocalDate.of(1, 6, 11))
         );
     }
 
@@ -49,23 +59,21 @@ public class ProjectServiceTest {
     }
 
     public void assertProject(PhaseType[] phaseTypes, int iterations) {
-        assertThat(projectDto).isNotNull();
         assertThat(projectDto.getPhases().size()).isEqualTo(phaseTypes.length);
         assertThat(projectDto.getIterations().size()).isEqualTo(iterations);
-        assertThat(projectDto.getPhases().stream().map(PhaseDto::getType).collect(Collectors.toList()))
-                .isEqualTo(Arrays.asList(PhaseType.Init, PhaseType.Elaboration, PhaseType.Construction, PhaseType.Transition));
     }
 
-    private void assertPhases(List<? extends Serializable>... expecteds) {
+    private void assertPhases(List<? extends Serializable>... expectedPhases) {
         for (PhaseType phaseType : PhaseType.values()) {
-            assertPhase(projectDto, expecteds[phaseType.ordinal()]);
+            assertPhase(projectDto, phaseType, expectedPhases[phaseType.ordinal()]);
         }
     }
 
-    private void assertPhase(ProjectDto projectDto, List<? extends Serializable> expected) {
-        assertThat(getPhase(projectDto, PhaseType.Init).getIterations().size()).isEqualTo((int) expected.get(0));
-        assertThat(getPhase(projectDto, PhaseType.Init).getStartDate()).isEqualTo(LocalDate.of(1, 1, 1));
-        assertThat(getPhase(projectDto, PhaseType.Init).getEndDate()).isEqualTo(LocalDate.of(1, 1, 18));
+    private void assertPhase(ProjectDto projectDto, PhaseType phaseType, List<? extends Serializable> expectedPhaseInformation) {
+        PhaseDto phase = getPhase(projectDto, phaseType);
+        assertThat(phase.getIterations().size()).isEqualTo((int) expectedPhaseInformation.get(0));
+        assertThat(phase.getStartDate()).isEqualTo(expectedPhaseInformation.get(1));
+        assertThat(phase.getEndDate()).isEqualTo(expectedPhaseInformation.get(2));
     }
 
     private PhaseDto getPhase(ProjectDto projectDto, PhaseType phaseType) {
@@ -75,6 +83,21 @@ public class ProjectServiceTest {
                 .orElse(null);
     }
 
+    private void assertIterations(List<? extends Serializable>... expectedIterations) {
+        for (List expectedIterationInfo : expectedIterations) {
+            assertIteration(projectDto, expectedIterationInfo);
+        }
+    }
+
+    private void assertIteration(ProjectDto projectDto, List expectedIterationInfo) {
+        assertThat(projectDto.getIterations()
+                .stream()
+                .anyMatch(iteration -> expectedIterationInfo.get(0)
+                        .equals(iteration.getStartDate()) && expectedIterationInfo.get(1)
+                        .equals(iteration.getEndDate()))).isTrue();
+
+    }
+
     private long cost(long cost) {
         return cost;
     }
@@ -82,25 +105,5 @@ public class ProjectServiceTest {
     private int iterations(int iterations) {
         return iterations;
     }
-
-//    @Test
-//    void planProjectTest_havingNumberOfIterations() {
-//        // Arrangement
-//        ProjectDto project = ProjectDto.builder()
-//                .startDate(LocalDate.of(1, 1, 1))
-//                .endDate(LocalDate.of(1, 6, 30))
-//                .numberOfIterations(12L)
-//                .cost(100000L)
-//                .build();
-//
-//        // Actions
-//        ProjectDto projectDto = sut.planProject(project);
-//
-//        // Asserts
-//        assertThat(projectDto).isNotNull();
-//        assertThat(projectDto.getPhases().size()).isEqualTo(4);
-//        assertThat(projectDto.getIterations().size()).isEqualTo(12);
-//
-//    }
 
 }
