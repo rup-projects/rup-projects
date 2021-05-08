98f3224094d7c58571384e1d1de24d756c6796a9
ruskab <aq0866sw1343de>
2021-05-08 21:21:06 +0200

    cypress e2e in github actions CI

--- web-app/jest.config.js
+++ web-app/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
   },
+  root: ["src"],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
