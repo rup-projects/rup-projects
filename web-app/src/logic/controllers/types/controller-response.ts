677ce77215de67f1a866a4298259d62dd0498bfb
Alberto Morales Fernandez <kodenix@gmail.com>
2021-09-20 11:31:04 +0200

    Added result tpes models

---
+++ web-app/src/logic/controllers/types/controller-response.ts
@@ -0,0 +1,26 @@
+import { AppError } from './app-error';
+
+export interface ControllerResponse<T> {
+  readonly status: ControllerResponseStatus;
+  readonly data: T;
+  readonly error: AppError;
+}
+
+export enum ControllerResponseStatus {
+  OK, ERROR
+}
+
+export class ControllerResponseImplementation<T> implements ControllerResponse<T> {
+  constructor(
+    public status: ControllerResponseStatus,
+    public data: T,
+    public error: AppError,
+  ) {
+    if (status === ControllerResponseStatus.OK) {
+      this.error = null;
+    }
+  }
+}
+
+
+
