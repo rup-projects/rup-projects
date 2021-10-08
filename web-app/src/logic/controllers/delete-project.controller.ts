929ffb5d146939f490fe3d0142cd65aab1f1315d
ruskab <ilya.kab@gmail.com>
2021-10-08 19:10:17 +0200

    delete project via API Rest operacion idempotente, realmente no haria falta comprobar antes si existe, y si no existe y le lanzamos la peticion DELETE, deberia devolver success 204 igualmente. Idempotente, siempre devuelve el mismo resultado si se lanza x veces https://youtu.be/p-qaNONn278?t=1388

--- web-app/src/logic/controllers/delete-project.controller.ts
+++ web-app/src/logic/controllers/delete-project.controller.ts
@@ -1,9 +1,8 @@
 import { Controller } from '../../commons/services/types/controller';
 import { ProjectRepository } from '../repositories/project.repository';
 import {Id} from '../../commons/model/id';
-import { ControllerResponse, ControllerResponseStatus } from './core/types/controller-response';
-import { Project } from '../models/project';
-import { AppError } from './core/types/app-error';
+import { ControllerResponse} from './core/types/controller-response';
+import {ControllerResponseFactory} from './core/controller-response.factory';
 
 export class DeleteProjectController implements Controller<Id, ControllerResponse<null>> {
 
@@ -12,33 +11,10 @@ export class DeleteProjectController implements Controller<Id, ControllerRespons
 
   async execute(id: Id): Promise<ControllerResponse<null>> {
     try {
-      /* TODO implementar getOne en el back para comprobar si existe el proyecto (Nunca borrar sin verificar)
-      const existentProject = await this.repository.getOne(id);
-      if (existentProject) {
-        const projectId = existentProjects[0].id;
-        await this.repository.delete(projectId);
-      }*/
       await this.repository.delete(id);
-      return this.createSuccessResponse();
+      return ControllerResponseFactory.createSuccess(null);
     } catch (e) {
-      return this.createFailResponse(e);
+      return ControllerResponseFactory.createFail(e);
     }
-  }
-
-  private createSuccessResponse(): ControllerResponse<null> {
-    return {
-      data: null,
-      status: ControllerResponseStatus.OK,
-    } as  ControllerResponse<null>;
-  }
-
-  private createFailResponse(systemError: Error): ControllerResponse<null> {
-    console.log('aqui4');
-    console.log(typeof systemError);
-    return {
-      data: null,
-      status: ControllerResponseStatus.ERROR,
-      error: { message: systemError.message } as AppError,
-    } as  ControllerResponse<null>;
   }
 }
