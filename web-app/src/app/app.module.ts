2c40295548906907b83abd9a9910a587d47faa8a
plfgavilan <plfgavilan.github.zone987@passmail.net>
2025-10-02 02:11:00 +0200

    Angular 18 upgrade completed!
    - Successfully upgraded from Angular 10 → 18
    - Updated Angular core packages from 17.3.12 to 18.2.14
    - Updated Angular CLI from 17.3.17 to 18.2.21
    - Applied HTTP modules migration to use provider functions
    - Now running the latest Angular version with modern features

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -20,23 +20,17 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   resourceTimelinePlugin
 ]);
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    BrowserModule,
-    CommonModule,
-    FullCalendarModule, // register FullCalendar with you app
-    HttpClientModule,
-    MatSnackBarModule,
-    SharedModule
-  ],
-  providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}
-  ],
-  exports: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent],
+    exports: [],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        CommonModule,
+        FullCalendarModule,
+        MatSnackBarModule,
+        SharedModule], providers: [
+        { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {
 }
